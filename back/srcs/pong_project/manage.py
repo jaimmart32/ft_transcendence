ec6274ee1fb4b4360cf9f8260f20b298fd4f6abe
Javier Sarabia Rodriguez <jsarabia@c3r12s1.42madrid.com>
2024-07-08 15:41:19 +0200

    creating http response and running a server with Django

--- back/srcs/pong_project/manage.py
+++ back/srcs/pong_project/manage.py
@@ -18,7 +18,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-    if request.method == "GET":
 
 
 if __name__ == '__main__':
--- back/srcs/pong_project/pong_project/settings.py
+++ back/srcs/pong_project/pong_project/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'pong_app',
+    # Our app
+    "pong_app",
 ]
 
 MIDDLEWARE = [
