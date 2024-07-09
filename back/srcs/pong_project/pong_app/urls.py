1128c67c4ce14248e969f1b27588048ec28c0d8b
Javier Sarabia Rodriguez <jsarabia@c3r12s1.42madrid.com>
2024-07-09 12:49:41 +0200

    testing with strings greeting the user

--- back/srcs/pong_project/pong_app/urls.py
+++ back/srcs/pong_project/pong_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("javi", views.index, name="index"),
+    path("<str:name>", views.greet, name="greet")
 ]
--- back/srcs/pong_project/pong_app/views.py
+++ back/srcs/pong_project/pong_app/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("hello from bbeltran, jaimmart and jsarabia!")
+    return render(request, "index.html")
+
+def index(request):
+    return HttpResponse("Hello, World!")
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name}!")
