87064ff5ad750d04286c8d8cd760359733924979
bbeltran <bbeltran@c1r1s4.42madrid.com>
2024-07-09 16:13:00 +0200

    Moved front to the backend container, added User model, created views for signup and login

--- back/srcs/pong_project/pong_app/models.py
+++ back/srcs/pong_project/pong_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
+#class	Usuario(models.Model):
+#	id = models.BigAutoField(primary_key=True)
+#	username = models.CharField(max_length=8, blank=True)
+#	email = models.CharField(max_length=320)
+#	password = models.CharField(max_length=132)
+	# Preferred language
+	# lang = models.CharField(max_length=2)
--- back/srcs/pong_project/pong_app/views.py
+++ back/srcs/pong_project/pong_app/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def main_view(request):
+	if request.method == 'GET'
+		return render(request, '../templates/pong_app/index.html')
+		
+def signup_view(request):
+	if request.method == 'POST'
+		data = json.loads(request.body)
+		email = data.get('email')
+		password = data.get('password')
+	
+		if User.objects.filter(email=email).exists():
+			return JsonResponse({'status': 'error', 'message': 'Email already in use'})
+		user = User.objects.create_user(email=email, password=password)
+		return JsonResponse({'status': 'success', 'message': 'User created succesfully!'})
+
+	return JsonResponse({'status': 'error', 'message': 'Invalid request'})
+
+def login_view(request):
+	if request.method == 'POST'
+		data = json.loads(request.body)
+		username = data.get('username')
+		password = data.get('password')
+	
+		if User.objects.filter(username=username).exists():
+			user = User.objects.get(username=username)
+			if user.password == password
+				return JsonResponse({'status': 'success', 'message': 'Logged in succesfully!'})
+			return JsonResponse({'status': 'error', 'message': 'Invalid password'})
+
+		return JsonResponse({'status': 'error', 'message': 'User does not exist'})
+
+	return JsonResponse({'status': 'error', 'message': 'Invalid request'})
