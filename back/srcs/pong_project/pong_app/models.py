6f3c7e30c329c1ccdc26b88c09c1bfbbec44188d
Javier Sarabia Rodriguez <jsarabia@c3r12s1.42madrid.com>
2024-07-11 12:49:09 +0200

    testing with database models

--- back/srcs/pong_project/pong_app/models.py
+++ back/srcs/pong_project/pong_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=16)
+    password = models.CharField(max_length=16)
+    games_played = models.IntegerField()
+    wins = models.IntegerField()
+    losses = models.IntegerField()
+    championships = models.IntegerField()
+    connected = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.username}"
+
+
+class Tournament(models.Model):
+    name = models.CharField(max_length=16)
+    winner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="winner")
+    runner_up = models.ForeignKey(User, on_delete=models.CASCADE, related_name="runner_up")
+
+    def __str__(self):
+        return f"{self.id}: {self.winner} beat {self.runner_up} to win {self.name}"
+
