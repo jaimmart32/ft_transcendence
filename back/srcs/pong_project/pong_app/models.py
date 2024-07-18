510976af12f530184d2aada47416824c1c089d14
Javier Sarabia Rodriguez <jsarabia@c3r12s1.42madrid.com>
2024-07-18 17:29:08 +0200

    database deleted

--- back/srcs/pong_project/pong_app/models.py
+++ back/srcs/pong_project/pong_app/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-class	CustomUser(AbstractUser):
+class CustomUser(AbstractUser):
 	# Preferred language
 	# lang = models.CharField(max_length=2)
 	tfa = models.BooleanField(default=False)
