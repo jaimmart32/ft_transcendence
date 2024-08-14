eb278fb2286320009f0cb1d26909e65f0da1459f
Jaime Martinez <jaimmart@c1r15s2.42madrid.com>
2024-08-14 12:38:32 +0200

    create_superuser script now creates 2 active users to ease testing

--- back/create_superuser.py
+++ back/create_superuser.py
@@ -24,3 +24,14 @@ if not User.objects.filter(username=username).exists():
     print(f'Superuser {username} created')
 else:
     print(f'Superuser {username} already exists')
+
+# Crea usuarios para disminuir el numero de acciones a la hora de testear
+from django.conf import settings
+
+contraseña = 'Password1' + settings.PEPPER
+user1 = User.objects.create_user(username='josito', email='josito@gmail.com', is_active=True)
+user2 = User.objects.create_user(username='pepita', email='pepita@gmail.com', is_active=True)
+user1.set_password(contraseña)
+user2.set_password(contraseña)
+user1.save()
+user2.save()
