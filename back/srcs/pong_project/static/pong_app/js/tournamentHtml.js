4fc222bd657017fbd309a36fe064c4ef3d0d12ff
bbeltran <bbeltran@c1r1s4.42madrid.com>
2024-11-01 17:25:42 +0100

    Joining a tournament now works, need to do more testing and finish its functionality.

--- back/srcs/pong_project/static/pong_app/js/tournamentHtml.js
+++ back/srcs/pong_project/static/pong_app/js/tournamentHtml.js
@@ -17,67 +17,6 @@ function createTournamentHTML()
 	`);
 }
 
-/*async function createTournament(tournamentName)
-{
-	const token = localStorage.getItem('access');
-    const uid = localStorage.getItem('userid');
-	if (token)
-	{
-        const TournamentInfo = 
-        {
-            user_id: uid,
-            tour_name: tournamentName,
-        };
-		try
-		{
-			const response = await fetch('/home/game/tournament/create/',
-			{
-				method: 'POST',
-				headers:
-				{
-					'Authorization': `Bearer ${token}`,
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify(TournamentInfo)
-			})
-			const data = await response.json();
-			if (data.status === 'success')
-			{
-                		alert('Tournament created successfully! Wait until it is full.');
-				navigateTo('/home/game/tournament/');
-	 		}
-			else
-			{
-                switch (data.message)
-                {
-					case 'A tournament with this name already exists.':
-						alert('This tournament name is already taken. Please choose a different name.');
-						break;
-					case 'User not found.':
-						alert('Your account was not found. Please log in again.');
-						navigateTo('/login/');
-						break;
-					case 'Invalid Tournament name.':
-						alert('Please enter a tournament name(between 2 and 15 characters).');
-						break;
-					default:
-						await checkRefresh(data, '/home/game/tournament/create/', token);
-				}
-			}
-		}
-		catch(error)
-		{
-			notAuthorized(error);
-		}
-	}
-	else
-	{
-		console.error('Error:', error);
-		alert('You are not authorized to view this page. Please log in.');
-		navigateTo('/login/');
-	}
-}*/
-
 function loadCreateTournament()
 {
 	const app = document.getElementById('app');
@@ -139,7 +78,7 @@ async function loadTournamentsSection()
 								<span class="friend-username">${tournament.name}</span>
 								<span class="friend-status">${tournament.players}/4</span>
 								<div class="friend-actions">
-									<button data-username="${tournament.name}" class="custom-button remove-friend" style="background-color: green; align-items: left;">
+									<button data-tournament="${tournament.name}" class="custom-button join-tournament" style="background-color: green; align-items: left;">
 										<i class="fas fa-plus"></i>
 									</button>
 								</div>
@@ -150,10 +89,13 @@ async function loadTournamentsSection()
 
 				app.innerHTML = tournamentsHTML;
 
-				document.querySelectorAll('.remove-friend').forEach(button => {
-					button.addEventListener('click', function(event) {
-						const username = event.target.getAttribute('data-username');
+				document.querySelectorAll('.join-tournament').forEach(button => {
+					button.addEventListener('click', function(event)
+					{
+						const tournamentName = event.currentTarget.getAttribute('data-tournament');
 						console.log("Someone clicked the button");
+						console.log(tournamentName);
+						joinTournament(tournamentName);
 						
 //						removeFriend(username);
 					});
@@ -178,6 +120,5 @@ async function loadTournamentsSection()
 }
 
 window.createTournamentHTML = createTournamentHTML;
-//window.createTournament = createTournament;
 window.loadTournamentsSection = loadTournamentsSection;
 window.loadCreateTournament = loadCreateTournament;
